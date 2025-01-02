3b27b929828bd8330a718a95ea34232554ff4af5
RonaldRis21 <ronald.tejada@oportunidades.org.sv>
2025-01-01 18:38:33 -0600

    fix: more specific validation for environment variables

--- nerdery-ecommerce/src/env.validation.ts
+++ nerdery-ecommerce/src/env.validation.ts
@@ -1,69 +1,88 @@
 import { plainToInstance } from 'class-transformer';
-import { IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, Min, MinLength } from 'class-validator';
 import { validateSync } from 'class-validator';
 
 class EnvironmentVariables {
   @IsString()
+  @IsNotEmpty()
   CLOUDINARY_NAME: string;
 
   @IsString()
+  @IsNotEmpty()
   CLOUDINARY_API_KEY: string;
 
   @IsString()
+  @IsNotEmpty()
   CLOUDINARY_API_SECRET: string;
 
   @IsString()
+  @IsNotEmpty()
   MAIL_HOST: string;
 
   @IsString()
+  @IsNotEmpty()
   MAIL_FROM: string;
 
   @IsString()
+  @IsNotEmpty()
   MAIL_USER: string;
 
   @IsString()
+  @IsNotEmpty()
   MAIL_PASS: string;
 
   @IsNumber()
+  @Min(1)
   PORT: number;
 
   @IsNumber()
   SECURITY_BCRYPT_SALT_OR_ROUND: number;
 
   @IsString()
+  @IsNotEmpty()
   JWT_EXPIRATION_IN: string;
 
   @IsString()
+  @IsNotEmpty()
   JWT_REFRESH_IN: string;
 
   @IsString()
+  @MinLength(30)
   JWT_ACCESS_SECRET: string;
 
   @IsString()
+  @MinLength(30)
   JWT_REFRESH_SECRET: string;
 
   @IsString()
+  @IsNotEmpty()
   FRONTEND_RESET_PASSWORD_URL: string;
 
   @IsString()
+  @IsNotEmpty()
   FRONTEND_PAYMENT_CLIENT_SECRET_URL: string;
 
   @IsString()
+  @IsNotEmpty()
   REDIS_HOST: string;
 
   @IsNumber()
+  @Min(1)
   REDIS_PORT: number;
 
   @IsString()
   NODE_ENV: string;
 
   @IsString()
+  @IsNotEmpty()
   DATABASE_URL: string;
 
   @IsString()
+  @IsNotEmpty()
   STRIPE_API_KEY: string;
 
   @IsString()
+  @IsNotEmpty()
   STRIPE_WEBHOOK_SECRET: string;
 }
 
