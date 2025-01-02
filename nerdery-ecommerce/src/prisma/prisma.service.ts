3ac31686f3bbd21eb6bf8995e4c74ca1362a61e3
RonaldRis21 <ronald.tejada@oportunidades.org.sv>
2025-01-01 18:37:52 -0600

    fix: Prisma reconnect when connection lost

--- nerdery-ecommerce/src/prisma/prisma.service.ts
+++ nerdery-ecommerce/src/prisma/prisma.service.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  private readonly maxRetries = 200;
   private readonly retryDelay = 3000;
   private readonly logger = new Logger(PrismaService.name);
 
@@ -26,16 +25,11 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     try {
       await this.$connect();
       this.logger.log('Connected to the database successfully!');
+      // eslint-disable-next-line unused-imports/no-unused-vars
     } catch (error) {
-      if (retries < this.maxRetries) {
-        this.logger.warn(
-          `Database connection failed. Retrying (${retries + 1}/${this.maxRetries})...`,
-        );
-        await new Promise((resolve) => setTimeout(resolve, this.retryDelay));
-        return this.connectWithRetry(retries + 1);
-      }
-      this.logger.error('Max retries reached. Could not connect to the database.', error);
-      throw error;
+      this.logger.error(`Database connection failed. Retrying (${retries + 1})...`);
+      await new Promise((resolve) => setTimeout(resolve, this.retryDelay));
+      return this.connectWithRetry(retries + 1);
     }
   }
 }
