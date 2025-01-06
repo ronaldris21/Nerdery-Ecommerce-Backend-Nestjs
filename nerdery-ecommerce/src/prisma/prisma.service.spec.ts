78588a1c20eddc7890522955bcf9a36fb8bbb162
RonaldRis21 <ronald.tejada@oportunidades.org.sv>
2025-01-06 16:34:53 -0600

    test: prisma retry connection tested

--- nerdery-ecommerce/src/prisma/prisma.service.spec.ts
+++ nerdery-ecommerce/src/prisma/prisma.service.spec.ts
@@ -1,19 +1,66 @@
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { PrismaService } from './prisma.service';
 
+const mockPrismaService = {
+  $connect: jest.fn(),
+  $disconnect: jest.fn(),
+  connectWithRetry: jest.fn(),
+};
+
 describe('PrismaService', () => {
   let service: PrismaService;
+  let logger: Logger;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+    jest.clearAllMocks();
+    jest.useRealTimers();
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [PrismaService],
+      // providers: [{ provide: PrismaService, useValue: mockPrismaService }],
     }).compile();
 
-    service = module.get<PrismaService>(PrismaService);
+    service = module.get(PrismaService);
+    logger = (service as any).logger;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('onModuleInit', async () => {
+    jest.spyOn(service, 'onModuleInit').mockResolvedValueOnce();
+
+    await service.onModuleInit();
+
+    expect(service.onModuleInit).toHaveBeenCalledTimes(1);
+  });
+
+  it('onModuleDestroy', async () => {
+    jest.spyOn(service, 'onModuleDestroy').mockResolvedValueOnce();
+
+    await service.onModuleDestroy();
+
+    expect(service.onModuleDestroy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Database connection in the first try', async () => {
+    jest
+      .spyOn(service, '$connect')
+      .mockRejectedValueOnce(new Error('Database connection failed. Retrying (1)...'))
+      .mockRejectedValueOnce(new Error('Database connection failed. Retrying (2)...'))
+      .mockResolvedValueOnce();
+
+    await service.connectWithRetry();
+
+    expect(service.$connect).toHaveBeenCalledTimes(3);
   });
 });
--- nerdery-ecommerce/src/prisma/prisma.service.ts
+++ nerdery-ecommerce/src/prisma/prisma.service.ts
@@ -21,14 +21,17 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     await this.$disconnect();
   }
 
-  private async connectWithRetry(retries = 0): Promise<void> {
+  async connectWithRetry(retries = 0): Promise<void> {
     try {
       await this.$connect();
+
       this.logger.log('Connected to the database successfully!');
       // eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars
     } catch (error) {
       this.logger.error(`Database connection failed. Retrying (${retries + 1})...`);
+
       await new Promise((resolve) => setTimeout(resolve, this.retryDelay));
+
       return this.connectWithRetry(retries + 1);
     }
   }
